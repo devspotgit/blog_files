3ac2d8cb18a99cae57c1e78c40134ff580e779d7
Patrick <patrick@devspot.ca>
2025-05-12 00:43:57 +0000

    changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,41 @@
 
 
 
+const resizeObserver = new ResizeObserver( entries => {
+    entries.forEach(entry => {
+        if(entry.target == document.querySelector(".main")){
+            if(entry.target.offsetWidth<=700 && ! entry.target.hasAttribute("mobile")){
+                entry.target.setAttribute("mobile", "")
+                // console.log("mobile attribute added")
+            }
+            else if(entry.target.offsetWidth > 700 && entry.target.hasAttribute("mobile")){
+                entry.target.removeAttribute("mobile")
+                // console.log("mobile attribute removed")
+            }
+        }
+    })
+})
+
+
+resizeObserver.observe(document.querySelector(".main"))
+
 
 document.querySelector(".side-open").addEventListener("click", ()=>{
-    document.querySelector(".side").style.left="0";
-    document.querySelector(".transparent").style.display="initial";
+    document.querySelector(".side").setAttribute("open", "")
+    document.querySelector(".transparent").setAttribute("show", "")
 })
-
 
 document.querySelector(".side-close").addEventListener("click", ()=>{
-    document.querySelector(".side").style.left="-250px";
-    document.querySelector(".transparent").style.display="none";
+    document.querySelector(".side").removeAttribute("open")
+    document.querySelector(".transparent").removeAttribute("show")
 })
-
 
 document.querySelector(".transparent").addEventListener("click", ()=>{
-    document.querySelector(".side").style.left="-250px";
-    document.querySelector(".transparent").style.display="none";
+    document.querySelector(".side").removeAttribute("open")
+    document.querySelector(".transparent").removeAttribute("show")
 })
+
+
+
 
 
